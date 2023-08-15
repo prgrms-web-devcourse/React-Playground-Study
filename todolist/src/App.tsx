5752d5eb86cf19c999ba9166cc9da7dfc33070d6
jgjgill <dbdltm22@naver.com>
2023-08-15 23:33:31 +0900

    chore: 임시 타이틀 추가

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <span className="material-symbols-outlined">search</span>
+    <div className="container">
+      <h1 className=" text-center text-2xl text-indigo-300">
+        <span className="material-symbols-outlined">lightbulb</span>여기는 준일팀이 함께 만들어가는 TodoList 공간입니다!
+      </h1>
     </div>
   )
 }
--- todolist/tailwind.config.js
+++ todolist/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
     fontFamily: {
-      Pretendard: ["Pretendard Variable", "Pretendard"],
+      Pretendard: ['Pretendard Variable', 'Pretendard'],
     },
   },
   plugins: [],
-};
+}
