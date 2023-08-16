a98b2712d75d2344327527beed96cbdeecbf9142
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-08-16 17:15:43 +0900

    feat: box 컴포넌트 생성

---
+++ storybook/src/components/Box.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Box = ({ width = 100, height = 100, backgroundColor = 'red' }) => {
+  const style = {
+    width,
+    height,
+    backgroundColor,
+  }
+  return <div style={style} />
+}
+
+export default Box
