3519bf189d60e699a0a011cbcacc74c98b47fda8
jkea1 <jinkyng7890@gmail.com>
2023-08-23 16:42:37 +0900

    feat: 편집버튼 누르면 커서 뜨게 하기

--- 정진경/react_ts_practice/typescript-tutorial/src/components/SingleTodo.tsx
+++ 정진경/react_ts_practice/typescript-tutorial/src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -33,13 +33,20 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     setEditMode(!editMode)
   }
 
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [editMode])
+
   return (
     <form className='todos__single' onSubmit={(e) => handleEdit(e, todo.id)}>
       {editMode ? (
         <input
           value={editTodo}
           onChange={(e) => setEditTodo(e.target.value)}
-          className='todo__single--test'
+          className='todos__single--text'
+          ref={inputRef}
         />
       ) : todo.isDone ? (
         <s className='todos__single--text'>{todo.todo}</s>
