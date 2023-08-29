4b85fba02af2a489b8e7fbe13ec37f7236326538
jkea1 <jinkyng7890@gmail.com>
2023-08-29 23:50:46 +0900

    refactor: handleAdd 기능

--- 정진경/react_ts_practice/typescript-tutorial/src/App.tsx
+++ 정진경/react_ts_practice/typescript-tutorial/src/App.tsx
@@ -1,26 +1,32 @@
-import React, { useState } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import './App.css'
-import InputFeild from './components/InputField'
+import InputField from './components/InputField'
 import TodoList from './components/TodoList'
 import { Todo } from './model'
 
 const App: React.FC = () => {
   // 굳이 꼭 React.FC를 사용하지 않고 인자에 각각 타입을 지정해 주는 방법 사용가능하다.
-  const [todo, setTodo] = useState<string>('') // 초기 상태 타입 지정
+  // const [todo, setTodo] = useState<string>('') // 초기 상태 타입 지정
   const [todos, setTodos] = useState<Todo[]>([])
 
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault()
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
-      setTodo('')
-    }
-  }
+  const handleAdd = useCallback(
+    (todo: string) => {
+      if (todo) {
+        setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+        console.log('handleAdd 기능 확인', todo, todos)
+      }
+    },
+    [todos]
+  )
+
+  useEffect(() => {
+    console.log(todos)
+  }, [todos])
 
   return (
     <div className='App'>
       <span className='heading'>🖇️ TODO 🖍️</span>
-      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <InputField handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
   )
