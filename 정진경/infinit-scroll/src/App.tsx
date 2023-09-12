ab3c3230b272d6991eeb465c5979b4d2a7b1ddf7
jkea1 <jinkyng7890@gmail.com>
2023-09-13 00:20:21 +0900

    커스텀 훅과 컴포넌트로 분리

--- 정진경/infinit-scroll/src/App.tsx
+++ 정진경/infinit-scroll/src/App.tsx
@@ -1,10 +1,6 @@
-import {
-  useInfiniteQuery,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { useEffect } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import axios from 'axios'
+import InfiniteScroll from './InfinitScroll/InfiniteScrollComponent'
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
@@ -26,57 +22,11 @@ function App() {
     return response
   }
 
-  const InfiniteScroll = () => {
-    const {
-      data = { pages: [], pageParams: undefined },
-      hasNextPage,
-      fetchNextPage,
-    } = useInfiniteQuery(['posts'], getPosts, {
-      getNextPageParam: (lastPage, allPages) => {
-        return allPages.length
-      },
-    })
-
-    useEffect(() => {
-      let fetching = false
-      const onScroll = async (event: any) => {
-        const { scrollHeight, scrollTop, clientHeight } =
-          event.target.scrollingElement
-        if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
-          fetching = true
-          if (hasNextPage) {
-            await fetchNextPage()
-          }
-          fetching = false
-        }
-      }
-      document.addEventListener('scroll', onScroll)
-      return () => {
-        document.removeEventListener('scroll', onScroll)
-      }
-    }, [data, hasNextPage, fetchNextPage])
-
-    return (
-      <div>
-        <ul>
-          {data.pages.map((page: any) =>
-            page.map((item: any) => (
-              <li style={{ marginBottom: '500px', fontSize: '20px' }}>
-                {item.title}
-              </li>
-            ))
-          )}
-        </ul>
-        <button onClick={() => fetchNextPage()}>페이지 불러오기</button>
-      </div>
-    )
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
       <div>
         <h1>포스트 리스트 목록</h1>
-        <InfiniteScroll />
+        <InfiniteScroll fetchData={getPosts} />
       </div>
     </QueryClientProvider>
   )
