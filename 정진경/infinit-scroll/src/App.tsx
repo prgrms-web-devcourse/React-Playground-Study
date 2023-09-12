5ad1ff95d929cda0dbbf55efae05509fd0737a9c
jkea1 <jinkyng7890@gmail.com>
2023-09-12 23:34:03 +0900

    무한 스크롤 기능 구현

--- 정진경/infinit-scroll/src/App.tsx
+++ 정진경/infinit-scroll/src/App.tsx
@@ -3,13 +3,15 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { useEffect } from 'react'
 import axios from 'axios'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+})
 
 function App() {
   const getPosts = async ({ pageParam = 0 }) => {
-    console.log('fetch')
     const response = await axios
       .get(
         'https://kdt.frontend.4th.programmers.co.kr:5011/posts/channel/64f843de36f4f3110a635033',
@@ -24,30 +26,45 @@ function App() {
     return response
   }
 
-  const InfiniteScrollComponent = () => {
-    const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
-      ['posts'],
-      getPosts,
-      {
-        getNextPageParam: (lastPage, allPages) => {
-          const currentPage = lastPage.pageParam + 1
-          if (allPages.some((page) => page.pageParam === currentPage)) {
-            return undefined
+  const InfiniteScroll = () => {
+    const {
+      data = { pages: [], pageParams: undefined },
+      hasNextPage,
+      fetchNextPage,
+    } = useInfiniteQuery(['posts'], getPosts, {
+      getNextPageParam: (lastPage, allPages) => {
+        return allPages.length
+      },
+    })
+
+    useEffect(() => {
+      let fetching = false
+      const onScroll = async (event: any) => {
+        const { scrollHeight, scrollTop, clientHeight } =
+          event.target.scrollingElement
+        if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+          fetching = true
+          if (hasNextPage) {
+            await fetchNextPage()
           }
-          return currentPage
-        },
+          fetching = false
+        }
       }
-    )
-    console.log(data)
+      document.addEventListener('scroll', onScroll)
+      return () => {
+        document.removeEventListener('scroll', onScroll)
+      }
+    }, [data, hasNextPage, fetchNextPage])
 
     return (
       <div>
         <ul>
-          {/* {data?.pages[0].map((page: any) => (
-            <li>{page.title}</li>
-          ))} */}
-          {data?.pages.map((page: any) =>
-            page?.data.map((post: any) => <li key={post._id}>{post.title}</li>)
+          {data.pages.map((page: any) =>
+            page.map((item: any) => (
+              <li style={{ marginBottom: '500px', fontSize: '20px' }}>
+                {item.title}
+              </li>
+            ))
           )}
         </ul>
         <button onClick={() => fetchNextPage()}>페이지 불러오기</button>
@@ -59,7 +76,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <div>
         <h1>포스트 리스트 목록</h1>
-        <InfiniteScrollComponent />
+        <InfiniteScroll />
       </div>
     </QueryClientProvider>
   )
