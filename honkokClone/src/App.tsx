3e04de7bda8da828cf0f33daa5ac71d098a2969e
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-09-20 00:36:09 +0900

    toast 연습 (#76)

--- honkokClone/src/App.tsx
+++ honkokClone/src/App.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-no-bind */
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
+import { useToast } from './components/toast/ToastProvider';
 
 const App = (): JSX.Element => {
   const { isLoading, error, data } = useQuery({
@@ -11,8 +13,17 @@ const App = (): JSX.Element => {
         .then((res) => res.data)
   });
 
+  const { addToast } = useToast();
+
   return (
     <div>
+      <div>
+        <h2 className="text-teal-500">토스트 만들기</h2>
+        <button onClick={() => addToast({ content: '토스트입니다.' })}>
+          토스트 추가
+        </button>
+      </div>
+
       <Link to="/detail">detail</Link>
       {isLoading ? (
         'Loading...'
--- honkokClone/src/main.tsx
+++ honkokClone/src/main.tsx
@@ -5,18 +5,21 @@ import './styles/index.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import DetailPage from './pages/DetailPage.tsx';
+import ToastProvider from './components/toast/ToastProvider.tsx';
 
 const queryClient: QueryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/detail" element={<DetailPage />} />
-        </Routes>
-      </BrowserRouter>
+      <ToastProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/detail" element={<DetailPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ToastProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
